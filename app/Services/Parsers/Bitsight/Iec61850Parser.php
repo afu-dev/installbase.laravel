e583b829898efe39c802e6e1076c1b7b72f2dd45
Adrien Furnari <adrien@furnari.fr>
2025-12-07 17:12:57 +0100

    fix(parsers): handled case with unknown command in iec data

--- app/Services/Parsers/Bitsight/Iec61850Parser.php
+++ app/Services/Parsers/Bitsight/Iec61850Parser.php
@@ -28,13 +28,21 @@ class Iec61850Parser extends AbstractJsonDataParser
 {
     protected function parseData(): array
     {
+        $vendor = $this->extractNested(["iec-61850", "Iec-61850"], "vendor")
+            ?? $this->extract(["Vendor", "vendor", "vendor_name"])
+            ?? "unknown";
+        $fingerprint = $this->extractNested(["iec-61850", "Iec-61850"], "product");
+        $version = $this->extractNested(["iec-61850", "Iec-61850"], "version");
+
+        $vendor = str_contains($vendor, "Unknown command") ? "unknown" : $vendor;
+        $fingerprint = str_contains($fingerprint, "Unknown command") ? null : $fingerprint;
+        $version = str_contains($version, "Unknown command") ? null : $version;
+
         return [
             new ParsedDeviceData(
-                vendor: $this->extractNested(["iec-61850", "Iec-61850"], "vendor")
-                    ?? $this->extract(["Vendor", "vendor", "vendor_name"])
-                    ?? "unknown",
-                fingerprint: $this->extractNested(["iec-61850", "Iec-61850"], "product"),
-                version: $this->extractNested(["iec-61850", "Iec-61850"], "version"),
+                vendor: $vendor,
+                fingerprint: $fingerprint,
+                version: $version,
             ),
         ];
     }
